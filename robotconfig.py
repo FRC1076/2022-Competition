ec9cf9d24eff75847104c9fe46a6bc8029eb04a0
Sophia Wang <s.sophi.w@gmail.com>
2022-03-09 16:22:33 -0500

    Reformat and fix obvious errors

--- robotconfig.py
+++ robotconfig.py
@@ -6,7 +6,6 @@ TANK = 2
 SWERVE = 3
 
 competition = {
-
     'CONTROLLERS': {
         'DRIVER': {
             'ID': 0,
@@ -36,7 +35,6 @@ competition = {
         'SHOOTER_ID': 5,
         'SHOOTER_RPM': 1000,
     },
-
     'INTAKE': {
         # Update IDs when known
         'INTAKE_MOTOR_ID': 6,
@@ -83,7 +81,6 @@ first_night = {
             'DEADZONE': DEADZONE,
             'LEFT_TRIGGER_AXIS': 2,
             'RIGHT_TRIGGER_AXIS': 3,
-
         },
         'OPERATOR': {
             'ID': 1,
@@ -115,7 +112,6 @@ first_night = {
         'CAMERA_HEIGHT': 4,
         'CAMERA_PITCH': 0,
     },
-
     'TILTSHOOTER': {
         'TILTSHOOTER_ID': 1,
         'ROTATIONS_PER_360': 75,
@@ -131,7 +127,7 @@ first_night = {
     'FEEDER': {
       'FEEDER_ID' : 9,
       'FEEDER_SPEED': 0.4,
-
+    },
     'CLIMBER': {
         'WINCH_LEFT_ID': 7,
         'WINCH_RIGHT_ID': 8,
@@ -141,14 +137,11 @@ first_night = {
         # DIO pin numbers
         'LEFT_LIMIT_ID': 0,
         'RIGHT_LIMIT_ID': 1,
-
         'CABLE_WRAPPED': 'UNDER',
-
-        # Both speeds positive. 
+        # Both speeds positive.
         # Extend speed must be lower than natural extend rate
         'EXTEND_SPEED': 0.5,
         'RETRACT_SPEED': 0.7,
-
     },
 }
 
@@ -178,7 +171,6 @@ practice = {
         },
         'DRIVETYPE': ARCADE
     },
-    
     'AIMER': {
         'AIMING_ROTATION_SPEED': 0.6,
         'AIMING_ACCURACY_DEGREES': 3,
@@ -204,7 +196,6 @@ intakeTest = {
             'DEADZONE': DEADZONE,
             'LEFT_TRIGGER_AXIS': 2,
             'RIGHT_TRIGGER_AXIS': 3,
-
         },
         'OPERATOR': {
             'ID': 1,
@@ -245,7 +236,6 @@ visionTest = {
             'DEADZONE': DEADZONE,
             'LEFT_TRIGGER_AXIS': 2,
             'RIGHT_TRIGGER_AXIS': 3,
-
         },
         'OPERATOR': {
             'ID': 1,
@@ -313,10 +303,4 @@ autonTest = {
     'VISION': 'enabled',
 }
 
-<<<<<<< HEAD
 robotconfig = first_night
-=======
-
-robotconfig = first_night
-
->>>>>>> cd557b5f13623c0f5d689d6b08e0164ab5693fef
