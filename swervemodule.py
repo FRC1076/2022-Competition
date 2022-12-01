2be7ea59989e43656c50eb51fde7fc5c5cd3a554
Ingmar-NT <ingmarisnt@gmail.com>
2022-11-30 19:15:04 -0500

    changed encoder references from voltages to angles

--- swervemodule.py
+++ swervemodule.py
@@ -23,13 +23,13 @@ class SwerveModule:
         self.rotateMotor = _rotateMotor
         self.cfg = _config
 
-        self.encoder = _rotateMotor.getEncoder()
+        self.encoder = _rotateMotor.getEncoder() # replace with CANcoder instantiation 
 
-        # Config
+        # Config -- change this to reflect how our config is formatted. We will upon testing of the entire drivetrain figure out which need to be inverted.
         self.sd_prefix = self.cfg.sd_prefix or 'Module'
-        self.encoder_zero = self.cfg.zero or 0
-        self.inverted = self.cfg.inverted or False
-        self.allow_reverse = self.cfg.allow_reverse or True
+        self.encoder_zero = self.cfg.zero or 0 #idk the point of this, maybe useful for other encoder type
+        self.inverted = self.cfg.inverted or False 
+        self.allow_reverse = self.cfg.allow_reverse or True #def allow reverse always, so you can maybe remove this
 
         # SmartDashboard
         self.sd = NetworkTables.getTable('SmartDashboard')
@@ -38,32 +38,33 @@ class SwerveModule:
         # Motor
         self.driveMotor.setInverted(self.inverted)
 
-        self._requested_voltage = 0
-        self._requested_speed = 0
+        self._requested_angle = 0 # change this to something like 'requested angle' or 'requested encoder value', whatever makes more sense
+        self._requested_speed = 0 #class variable which execute() passes to the drive motor at the end of the robot loop
 
         # PID Controller
         # kP = 1.5, kI = 0.0, kD = 0.0
-        self._pid_controller = PIDController(1.5, 0.0, 0.0)
-        self._pid_controller.enableContinuousInput(0.0, 5.0) # Will set the 0 and 5 as the same point
-        self._pid_controller.setTolerance(0.05, 0.05) # Tolerance where the PID will be accpeted aligned
+        self._pid_controller = PIDController(1.5, 0.0, 0.0) #swap this stuff for CANSparkMax pid controller -- see example from last year shooter
+        self._pid_controller.enableContinuousInput(0.0, 5.0) # Make this range -1 to 1 instead
+        self._pid_controller.setTolerance(0.05, 0.05) # may need to tweak this with PID testing
 
-    def get_voltage(self):
+    def get_ticks(self):
         """
         :returns: the voltage position after the zero
         """
-        return self.encoder.getAverageVoltage() - self.encoder_zero
+        return self.encoder.getAbsolutePosition() - self.encoder_zero
 
-    def flush(self):
+    def flush(self): # rewrite this (although it isnt used anywhere) to reset the encoder to 0 and zero out the speed, if you want.
         """
         Flush the modules requested speed and voltage.
         Resets the PID controller.
         """
-        self._requested_voltage = self.encoder_zero
+        self._requested_angle = self.encoder_zero
         self._requested_speed = 0
         self._pid_controller.reset()
 
+    
     @staticmethod
-    def voltage_to_degrees(voltage):
+    def voltage_to_degrees(voltage): #make this like TICKS TO DEG or smth
         """
         Convert a given voltage value to degrees.
         :param voltage: a voltage value between 0 and 5
@@ -77,7 +78,7 @@ class SwerveModule:
         return deg
 
     @staticmethod
-    def voltage_to_rad(voltage):
+    def voltage_to_rad(voltage): #same, but TICKS TO RAD
         """
         Convert a given voltage value to rad.
         :param voltage: a voltage value between 0 and 5
@@ -86,7 +87,7 @@ class SwerveModule:
         return (voltage / 5) * 2 * math.pi
 
     @staticmethod
-    def degree_to_voltage(degree):
+    def degree_to_voltage(degree): #DEG TO TICK
         """
         Convert a given degree to voltage.
         :param degree: a degree value between 0 and 360
@@ -94,39 +95,39 @@ class SwerveModule:
         """
         return (degree / 360) * 5
 
-    def _set_deg(self, value):
+    def _set_deg(self, value): #This one weird. Dont do mod stuff if possible - it messes up PID calculations
         """
         Round the value to within 360. Set the requested rotate position (requested voltage).
         Intended to be used only by the move function.
         """
-        self._requested_voltage = ((self.degree_to_voltage(value) + self.encoder_zero) % 5)
+        self._requested_angle = (value + self.encoder_zero) % 360
 
-    def move(self, speed, deg):
+    def move(self, speed, deg): #this is all good mostly
         """
         Set the requested speed and rotation of passed.
         :param speed: requested speed of wheel from -1 to 1
         :param deg: requested angle of wheel from 0 to 359 (Will wrap if over or under)
         """
-        deg %= 360 # Prevent values past 360
+        deg %= 360 # mod 360, may want to change
 
-        if self.allow_reverse:
+        if self.allow_reverse: #addresses module-flipping
             """
             If the difference between the requested degree and the current degree is
             more than 90 degrees, don't turn the wheel 180 degrees. Instead reverse the speed.
             """
-            if abs(deg - self.voltage_to_degrees(self.get_voltage())) > 90:
+            if abs(deg - self.get_ticks()) > 90: #make this with the new tick-degree methods
                 speed *= -1
                 deg += 180
                 deg %= 360
 
-        self._requested_speed = speed
+        self._requested_speed = speed 
         self._set_deg(deg)
 
-    def debug(self):
+    def debug(self): #can use logging/SD if useful
         """
         Print debugging information about the module to the log.
         """
-        print(self.sd_prefix, '; requested_speed: ', self._requested_speed, ' requested_voltage: ', self._requested_voltage)
+        print(self.sd_prefix, '; requested_speed: ', self._requested_speed, ' requested_angle: ', self._requested_angle)
 
     def execute(self):
         """
@@ -136,20 +137,22 @@ class SwerveModule:
         """
         # Calculate the error using the current voltage and the requested voltage.
         # DO NOT use the #self.get_voltage function here. It has to be the raw voltage.
-        error = self._pid_controller.calculate(self.encoder.getVoltage(), self._requested_voltage)
+        error = self._pid_controller.calculate(self.get_ticks(), self._requested_angle) #Make this an error in ticks instead of voltage
 
         # Set the output 0 as the default value
         output = 0
         # If the error is not tolerable, set the output to the error.
+
         # Else, the output will stay at zero.
         if not self._pid_controller.atSetpoint():
-            # Use max-min to clamped the output between -1 and 1.
+            # Use max-min to clamped the output between -1 and 1. The CANSparkMax PID controller does this automatically, so idk if this is necessary
             output = max(min(error, 1), -1)
 
         # Put the output to the dashboard
         self.sd.putNumber('drive/%s/output' % self.sd_prefix, output)
         # Set the output as the rotateMotor's voltage
-        self.rotateMotor.set(output)
+        self.rotateMotor.set(output) # will replace this with a set SETPOINT rather than actually setting the speed
+        #SparkMax PID controller will take care of actually running the motors with PID values you instantiate it with
 
         # Set the requested speed as the driveMotor's voltage
         self.driveMotor.set(self._requested_speed)
@@ -160,13 +163,12 @@ class SwerveModule:
         """
         Output a bunch on internal variables for debugging purposes.
         """
-        self.sd.putNumber('drive/%s/degrees' % self.sd_prefix, self.voltage_to_degrees(self.get_voltage()))
+        self.sd.putNumber('drive/%s/degrees' % self.sd_prefix, self.get_ticks())
 
         if self.debugging.getBoolean(False):
-            self.sd.putNumber('drive/%s/requested_voltage' % self.sd_prefix, self._requested_voltage)
+            self.sd.putNumber('drive/%s/requested_voltage' % self.sd_prefix, self._requested_angle)
             self.sd.putNumber('drive/%s/requested_speed' % self.sd_prefix, self._requested_speed)
-            self.sd.putNumber('drive/%s/raw voltage' % self.sd_prefix, self.encoder.getVoltage())  # DO NOT USE self.get_voltage() here
-            self.sd.putNumber('drive/%s/average voltage' % self.sd_prefix, self.encoder.getAverageVoltage())
+            self.sd.putNumber('drive/%s/encoder position' % self.sd_prefix, self.encoder.getAbsolutePosition())
             self.sd.putNumber('drive/%s/encoder_zero' % self.sd_prefix, self.encoder_zero)
 
             self.sd.putNumber('drive/%s/PID Setpoint' % self.sd_prefix, self._pid_controller.getSetpoint())
